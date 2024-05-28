af9bf5ac42c4e32f84487dd776a33e43cf85fbb5
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-05-28 19:08:22 +0600

    able

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
-  authDomain: process.envAUTH_DOMAIN.,
+  authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
